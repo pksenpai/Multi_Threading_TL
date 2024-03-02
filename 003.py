0e56cf7832c1357ac29360ce597223cb8790d2d8
PKPY <p01001011p01011001@gmail.com>
2024-03-02 19:13:50 +0330

    [003]: coded and test current_thread & enumerate in treading.

---
+++ 003.py
@@ -0,0 +1,43 @@
+""" current_thread & enumerate """
+from time import sleep, perf_counter
+from threading import Thread, current_thread, enumerate
+
+
+"""\_____[current_thread]_____/"""  
+start = perf_counter()
+
+def show(name):
+    print(f'Start {name}!')
+    
+    # show all alive threads
+    print(enumerate())
+    
+    # thread1 & thread2 repr
+    print(current_thread()) 
+
+    # thread1 & thread2 _name
+    # print(current_thread().getName())
+    
+    # thread1 & thread2 id
+    # print(current_thread().ident)
+        
+    sleep(3)
+    print(f'End {name}!')
+
+""" object create """
+# Tip: you can chage the self._name of Thread class with name param!
+t1 = Thread(target=show, args=('One',), name='First')
+t2 = Thread(target=show, args=('Two',), name='Second')
+
+""" start threading """
+t1.start()
+t2.start()
+print(current_thread()) # MainThread
+
+""" wait to finish thread """
+# commented for test daemon
+t1.join()
+t2.join()
+
+end = perf_counter()
+print(f"{round(end - start)}s")
